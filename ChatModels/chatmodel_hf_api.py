9b68e84cff9064279dd65fa45b878e3ef2211fb3
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2026-06-04 13:31:04 +0530

    Add Hugging Face API Model

---
+++ ChatModels/chatmodel_hf_api.py
@@ -0,0 +1,15 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation",
+)
+
+chat = ChatHuggingFace(llm=llm)
+
+response = chat.invoke("What is the capital of India?")
+
+print(response)
