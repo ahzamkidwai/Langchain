9ea3a42bc8790d74d98b6fd4bdf0a67b4464ab6a
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2026-06-04 14:46:46 +0530

    Use Hugging Face Model Locally by installing on your machine

---
+++ ChatModels/chatmodel_hf_local.py
@@ -0,0 +1,15 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+
+llm = HuggingFacePipeline.from_model_id(
+    model_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation",
+    pipeline_kwargs={
+        "max_new_tokens": 100,
+        "temperature": 0.3,
+    }
+)
+
+model = ChatHuggingFace(llm=llm)
+
+response = model.invoke("Explain Python in one sentence.")
+print(response.content)
