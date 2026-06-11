a2a3c462ee3361a5164d11370a3e5186fbd356d9
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2026-06-12 00:43:56 +0530

    Add chat history for context

--- Chatbot/chatbot.py
+++ Chatbot/chatbot.py
@@ -5,13 +5,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 model = ChatGoogleGenerativeAI(model="gemini-3.5-flash")
-# model = ChatGroq(model="openai/gpt-oss-20b")
-
+chat_history = []
 
 while True:
     user_input = input("You : ")
+    chat_history.append(user_input)
     if user_input == "exit":
         break
-    result = model.invoke(user_input)
+    result = model.invoke(chat_history)
     ans = result.content[0]["text"]
+    chat_history.append(ans)
     print("AI : ", ans)
+
+print("Chat History : ", chat_history)
