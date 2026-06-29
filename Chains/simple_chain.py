36ef0e927dc015b41c88d6555f88e479b58c8949
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2026-06-29 18:05:01 +0530

    Add Chains: Simple Chain

---
+++ Chains/simple_chain.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+prompt = PromptTemplate(
+    template="Generate 5 interesting facts about {topic}",
+    input_variables=["topic"]
+)
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.7
+)
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+
+response = chain.invoke({"topic": "cricket"})
+
+print(response)
+
+chain.get_graph().print_ascii()
