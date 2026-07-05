a34c62ac6d90ee4e636fe0476ca9c1acc8fb4785
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2026-07-05 13:27:57 +0530

    [Runnables]: Add Runnables Sequence

---
+++ Runnables/runnable_sequence.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template="Write a joke about {topic}",
+    input_variables=["topic"]
+)
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.7
+)
+
+parser = StrOutputParser()
+
+prompt2 = PromptTemplate(
+    template="Explain the following joke in detail - ${text}",
+    input_variables=["text"]
+)
+
+chain = prompt1 | model | parser | prompt2 | model | parser
+
+result = chain.invoke({"topic": "AI"})
+print(result)
