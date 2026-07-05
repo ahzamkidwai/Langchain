b282cad6bb15f3f6a25278c1b7ababecfd3c9b73
ahzamkidwai <ahzamnaseem.kidwai@gmail.com>
2026-07-05 14:19:47 +0530

    [Runnables]: RunnableLamda

---
+++ Runnables/runnable_lamda.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableSequence, RunnableParallel, RunnablePassthrough, RunnableLambda
+
+load_dotenv()
+
+def word_count(text):
+    return len(text.split())
+
+prompt = PromptTemplate(
+    template="Write a joke about {topic}",
+    input_variables=["topic"]
+)
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.7
+)
+
+parser = StrOutputParser()
+
+joke_generate_chain = RunnableSequence(prompt, model, parser)
+
+parallel_chain = RunnableParallel({
+    'joke': RunnablePassthrough(),
+    'word_count': RunnableLambda(word_count)
+})
+
+final_chain = RunnableSequence(joke_generate_chain, parallel_chain)
+
+result = final_chain.invoke({'topic': 'Machine Learning'})
+print('\n\n result : \n', result)
